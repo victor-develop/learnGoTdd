e0724f015871761c3aa6ce6282525f5bb4cff93c
Victor XH <victor.develop@outlook.com>
2021-12-28 09:20:14 +0000

    :art: Add default behavior for empty string

--- hello.go
+++ hello.go
@@ -5,10 +5,12 @@ import "fmt"
 const enlglishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+  if name == "" {
+    name = "World"
+  }
   return enlglishHelloPrefix + name
 }
 
 func main() {
   fmt.Println(Hello("Chris"))
 }
-

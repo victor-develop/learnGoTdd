b79040b6dbf9e8851e95760e481c3fdd01bf5b35
Victor XH <victor.develop@outlook.com>
2021-12-28 09:30:07 +0000

    :sparkles: Add Spanish arg

--- hello.go
+++ hello.go
@@ -4,13 +4,18 @@ import "fmt"
 
 const enlglishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
   if name == "" {
     name = "World"
   }
+
+  if language == "Spanish" {
+    return "Hola, " + name
+  }
+
   return enlglishHelloPrefix + name
 }
 
 func main() {
-  fmt.Println(Hello("Chris"))
+  fmt.Println(Hello("Chris", ""))
 }

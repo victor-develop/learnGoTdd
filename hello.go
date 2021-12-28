8573a6ecda99814d7ef5114530085e77b4ea27e9
Victor XH <victor.develop@outlook.com>
2021-12-28 09:39:01 +0000

    :art: Use switch for conditions

--- hello.go
+++ hello.go
@@ -3,17 +3,23 @@ package main
 import "fmt"
 
 const enlglishHelloPrefix = "Hello, "
+const french = "French"
+const spanish = "Spanish"
 
 func Hello(name string, language string) string {
   if name == "" {
     name = "World"
   }
 
-  if language == "Spanish" {
-    return "Hola, " + name
+  prefix := enlglishHelloPrefix
+  
+  switch language {
+    case french:
+      prefix = "Bonjour, "
+    case spanish:
+      prefix = "Hola, "
   }
-
-  return enlglishHelloPrefix + name
+  return prefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -23,9 +23,15 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
   })
 
-  t.Run("in Spanish", func(t *testing.T){
+  t.Run("in Spanish", func(t *testing.T) {
     got := Hello("Elodie", "Spanish")
     want := "Hola, Elodie"
+    assertCorrectMessage(t, got, want)
+  })
+
+  t.Run("in French", func(t *testing.T) {
+    got := Hello("Elodie", "French")
+    want := "Bonjour, Elodie"
     assertCorrectMessage(t, got, want)
   })
 }
